ab74527a4f91f5455c3dfc8ca7ddd8c72f3d407c
vandalv <vandalv@tut.by>
2020-09-09 23:10:22 +0200

    init

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let activePlayer = 0;
 let prevDice = 0;
 let dice = 0;
 
-console.log('Hello!');
+console.log('Hello app!');
 
 function nextPlayer() {
   document.querySelector('.btn-hold').style.display = 'none';
